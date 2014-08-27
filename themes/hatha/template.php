2c604efd7bddddead3a51b9fc70c021b5a5693c9
Haugen <tobiashaugen@gmail.com>
2014-08-27 16:14:02 +0200

    Remove pane-separator div

--- themes/hatha/template.php
+++ themes/hatha/template.php
@@ -3,3 +3,14 @@
  * @file
  * Template file
  */
+
+/**
+ * Override panels function to remove pane-separator div.
+ */
+function hatha_panels_default_style_render_region($vars) {
+  $output = '';
+  foreach ($vars['panes'] as $pane) {
+    $output .= $pane;
+  }
+  return $output;
+}
